5db013b6630cc6385b25ca57ef6e5039ba885d6c
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-05-12 13:24:38 +0300

    add json respose endpoint

--- web/app.py
+++ web/app.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Flask Dockerized with live changes'
+
+@app.route('/api/availability/station/<int:stationId>/time/<dateAndHour>', methods=['GET'])
+def availability(stationId, dateAndHour):
+    return jsonify(avlbikes=5,
+                   date=dateAndHour,
+                   stationId=stationId), 200
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
